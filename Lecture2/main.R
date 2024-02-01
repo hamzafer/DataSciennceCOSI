07b44d0a27fa21b12958105136885ea7e5eaddf4
Hamza Zafar <hamzafer3@gmail.com>
2024-02-01 11:00:55 -0500

    Polynomial Regression

--- Lecture2/main.R
+++ Lecture2/main.R
@@ -5,3 +5,8 @@ summary(model)
 # Non additive linear regression
 model1 <- lm(hp ~ cyl * carb, mtcars)
 summary(model1)
+
+# Polynomial Regression
+model2 <- lm(mpg ~ hp + I(hp^2), mtcars)
+summary(model2)
+
