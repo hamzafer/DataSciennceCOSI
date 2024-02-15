a0dc669f973bca5db49be5829f95324b72d7158d
Hamza Zafar <hamzafer3@gmail.com>
2024-02-15 12:47:40 -0500

    Activity 5 done

---
+++ Activity5/main.R
@@ -0,0 +1,23 @@
+# Apply the KNN imputation technique on the biopsy (MASS) dataset to estimate missing values.
+library(MASS)
+data(biopsy)
+library(DMwR2)
+set.seed(2)
+
+missing_values <- sapply(biopsy, function(x) any(is.na(x)))
+print(missing_values)
+# From the output, we can see that the V6 column contains missing values.
+
+# Create a copy of biopsy with missing values
+biopsy.missing <- biopsy
+selected <- sample(nrow(biopsy.missing), 5)
+biopsy.missing[selected, 'V6'] <- NA
+
+# Perform KNN imputation
+biopsy.filled <- knnImputation(biopsy.missing, k = 5)  # uses weighted average and scales by default
+
+# Calculate relative differences between real and estimated values
+relative_differences <- (biopsy[selected, 'V6'] - biopsy.filled[selected, 'V6']) / biopsy[selected, 'V6']
+
+# Print the relative differences
+print(relative_differences)
