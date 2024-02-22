0584d262c135ded84a2af71aa1096e308ca7514b
Hamza Zafar <hamzafer3@gmail.com>
2024-02-22 13:00:35 -0500

    Activity 6 done

--- Activity6/submission.R
+++ Activity6/submission.R
@@ -1,65 +1,44 @@
 library(MASS)
 library(ISLR)
-library(ROCR)
 library(class)
-library(DMwR2)
-data("Default")
+library(caret)
+library(ROCR)
 
-set.seed(2)
-Default2 <- na.omit(Default)
-# Make "No" the positive default
-Default2$default <- factor(Default2$default, c("No", "Yes"))
+data("Default", package = "ISLR")
+Default$default <- ifelse(Default$default == "Yes", 1, 0)
+Default$student <- ifelse(Default$student == "Yes", 1, 0)
 
-# Each of nine attributes has been scored on a scale of 1 to 10
-# Just in case
-names <- names(Default2)
-scaled <- data.frame(Default2[, 1:2], scale(Default2[, 3:4]))
-Default2 <- scaled
-names(Default2) <- names
+set.seed(123)
+trainingIndex <- createDataPartition(Default$default, p = 0.7, list = FALSE)
+trainingData <- Default[trainingIndex,]
+testData <- Default[-trainingIndex,]
+trainingData_scaled <- scale(trainingData[, -1])
+testData_scaled <- scale(testData[, -1], center = attr(trainingData_scaled, "scaled:center"), scale = attr(trainingData_scaled, "scaled:scale"))
 
-train <- sample(nrow(Default2), nrow(Default2) / 2, replace = FALSE)
-train.set <- Default2[train,]
-test.set <- Default2[-train,]
+set.seed(123)
+knn_pred_3 <- knn(train = trainingData_scaled, test = testData_scaled, cl = trainingData$default, k = 3)
+knn_pred_5 <- knn(train = trainingData_scaled, test = testData_scaled, cl = trainingData$default, k = 5)
 
-##
+accuracy_k3 <- sum(testData$default == knn_pred_3) / length(knn_pred_3)
+accuracy_k5 <- sum(testData$default == knn_pred_5) / length(knn_pred_5)
 
-# Prior probabilities for each default.
-total <- sum(table(Default2$default))
-prior.prob <- c(table(Default2$default)[1] / total, table(Default2$default)[2] / total)
+cat("Accuracy for K=3:", accuracy_k3, "\n")
+cat("Accuracy for K=5:", accuracy_k5, "\n")
 
-###
-train.set$default <- as.factor(train.set$default == "No")
-test.set$default <- as.factor(test.set$default == "No")
-
-k1 <- 5
-k2 <- 10
-
-knn.pred1 <- knn(train = train.set[, -1], test = test.set[, -1], cl = train.set[, 1], k = k1)
-knn.pred2 <- knn(train = train.set[, -1], test = test.set[, -1], cl = train.set[, 1], k = k2)
-
-####
-
-# Get probabilities of positive default
-adapted.pred1 <- knn.pred1$posterior[, 1] # Probability of positive default
-adapted.pred2 <- knn.pred2$posterior[, 1] # Probability of positive default
-# ROCR requirement lowest level -> negative default
-adapted.labels <- ifelse(test.set$default == "No", 1, 0)
-
-###
-
-assessModel <- function(adapted.pred, adapted.labels)
-{
-  pred <- prediction(adapted.pred, adapted.labels)
-  plot(performance(pred, 'tpr', 'fpr'))
-  # print(performance(pred, 'tpr', 'fpr'))
+assessModel <- function(predictions, labels) {
+  pred <- prediction(predictions, labels)
+  plot(performance(pred, 'tpr', 'fpr'), colorize = TRUE)
   auc <- performance(pred, measure = "auc")
-  print(attr(auc, "y.values")[[1]])
-  # TPR, FPR and cutoff values
-  # print(performance(pred, measure = "tpr")@y.values[[1]])
-  # print(performance(pred, measure = "fpr")@y.values[[1]])
-  # print(performance(pred, 'tpr', 'fpr')@alpha.values[[1]])
-  print("______________")
+  cat("AUC:", attr(auc, "y.values")[[1]], "\n")
+  cat("______________\n")
 }
 
-assessModel(adapted.pred1, adapted.labels)
-assessModel(adapted.pred2, adapted.labels)
+knn_pred_3_numeric <- as.numeric(knn_pred_3) - 1 # Assuming your factors are "1" and "2"
+knn_pred_5_numeric <- as.numeric(knn_pred_5) - 1
+true_labels <- testData$default
+
+cat("Model with K=3\n")
+assessModel(knn_pred_3_numeric, true_labels)
+
+cat("\nModel with K=5\n")
+assessModel(knn_pred_5_numeric, true_labels)
