c984e9d52f60aec41eb8de30a83599a3ca0bbb41
Hamza Zafar <hamzafer3@gmail.com>
2024-04-02 12:38:41 +0200

    DS Part 2 Starts.

--- Part2/Lecture1/main2.R
+++ Part2/Lecture1/main2.R
@@ -8,3 +8,7 @@ iris.tree <- tree(Species ~ ., iris)
 plot(iris.tree)
 text(iris.tree)
 iris.tree
+
+# means that the setosa is just easy to classify
+# also the 4th variable is not even used in the tree
+# thats why trees are useful for feature selection
