7bd175c358c942697a1e5e21fe7f248f3fedfa5c
Hamza Zafar <hamzafer3@gmail.com>
2024-04-18 11:39:56 +0200

    DS Part 2 N nets.

--- Part2/Lecture5/nn.R
+++ Part2/Lecture5/nn.R
@@ -9,8 +9,16 @@ summary(nn.iris)
 nn.iris.predict.train <- predict(nn.iris, iris[train,], type = "class")
 nn.iris.predict.train <- as.factor(nn.iris.predict.train)
 table(nn.iris.predict.train, iris[train, "Species"])
+confusion.train <- table(nn.iris.predict.train, iris[train, "Species"])
+accuracy.train <- sum(diag(confusion.train)) / sum(confusion.train)
 
 nn.iris.predict.test <- predict(nn.iris, iris[-train,], type = "class")
 nn.iris.predict.test <- as.factor(nn.iris.predict.test)
 table(nn.iris.predict.test, iris[-train, "Species"])
+confusion.test <- table(nn.iris.predict.test, iris[-train, "Species"])
+accuracy.test <- sum(diag(confusion.test)) / sum(confusion.test)
+
+# Print accuracies
+cat("Accuracy on training set:", accuracy.train, "\n")
+cat("Accuracy on test set:", accuracy.test, "\n")
 
